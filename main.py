6bccf71b9aaaa37db703ff413199cbc13fa748ac
Sebastián Del Moral <sebmorgal@gmail.com>
2023-10-29 11:53:26 +0100

    feat: Use serial port instead usb to receive panel data

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ import micropython
 import led_panel_handler
 import link_layer
 import cmd_layer
+import machine
+import time
 
 # -----------------------------------------------------------------------------
+
+DEFAULT_SERIAL_PORT = 1
+DEFAULT_SERIAL_TX_PIN = 8
+DEFAULT_SERIAL_RX_PIN = 9
+DEFAULT_BAUDRATE = 921600
+DEFAULT_RX_BUFF_SIZE = 4*1024
 
 class DeviceHandler:
 
@@ -17,12 +25,14 @@ class DeviceHandler:
         self.__link_handler = link_layer.LinkHandler(frame_callback=self.__link_callback, send_func=self.__link_write)
         self.__cmd_handler = cmd_layer.CommandHandler(cmd_callback=self.__cmd_callback, send_func=self.__cmd_write)
         self.__led_panel = led_panel_handler.LedPanel()
+        self.__uart = machine.UART(DEFAULT_SERIAL_PORT)
+        self.__uart.init(baudrate=DEFAULT_BAUDRATE, rx=machine.Pin(DEFAULT_SERIAL_RX_PIN), tx=machine.Pin(DEFAULT_SERIAL_TX_PIN), rxbuf=DEFAULT_RX_BUFF_SIZE)
 
     def __link_callback(self, link_frame:bytearray):
         self.__cmd_handler.process_frame(link_frame)
 
     def __link_write(self, data_link:bytearray):
-        sys.stdout.buffer.write(data_link)
+        self.__uart.write(data_link)
 
     def __cmd_callback(self, cmd_payload:cmd_layer.CommandPayload):
         if cmd_payload.get_cmd() == cmd_layer.CMD_ID_ACK:
@@ -47,9 +57,10 @@ class DeviceHandler:
     def __cmd_write(self, data_cmd:bytearray):
         self.__link_handler.send_frame(data_cmd)
 
-    def process_byte(self, data_byte:int):
-        self.__link_handler.get_byte(data_byte)
-
+    def check_port(self):
+        while self.__uart.any() > 0:
+            self.__link_handler.get_bytes(self.__uart.read(self.__uart.any()))
+            
 
 # -----------------------------------------------------------------------------
 
@@ -59,9 +70,6 @@ def enable_repl_scape(status:bool):
     else:
         micropython.kbd_intr(-1)
 
-def read_byte():
-    return sys.stdin.buffer.read(1)
-
 # -----------------------------------------------------------------------------
 
 device = DeviceHandler()
@@ -70,14 +78,9 @@ device = DeviceHandler()
 
 def main(arguments):
 
-    # Disable scape character on REPL
-    enable_repl_scape(False)
-
     # Main loop
     while True:
-        c = read_byte()
-        if c != None:
-            device.process_byte(c)
+        device.check_port()
 
 
 if __name__ == '__main__':
